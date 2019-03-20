fcfc5b8d7bb21b6d16192e3229a58cec58220f6d
Bailey Blum <bblum1@nd.edu>
2019-03-19 19:46:27 -0700

    *should* read all packets and print size, but gets stuck in loop

--- pcapHash.c
+++ pcapHash.c
@@ -1,5 +1,5 @@
 // Ale Lopez, Bailey Blum, and Horacio Lopez
-// CSE34341 - Project4
+// CSE34341 - Project 4
 // Due: 3/28/2019
 
 #include <stdlib.h>
@@ -7,6 +7,41 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+
+void DumpAllPacketLengths (FILE *fp)
+{
+    /* We are going to assume that fp is just after the global header */
+    uint32_t     nPacketLength;
+    char         theData[2000];
+ 
+    while(!feof(fp)) {
+        /* Skip the ts_sec field */
+        fseek(fp, 4, SEEK_CUR);   
+ 
+        /* Skip the ts_usec field */
+        fseek(fp, 4, SEEK_CUR);
+ 
+        /* Read in the incl_len field */
+        fread(&nPacketLength, 4, 1, fp);
+ 
+        /* Skip the orig_len field */
+        fseek(fp, 4, SEEK_CUR);
+ 
+        /* Let’s do a sanity check before reading */
+        if(nPacketLength > 2000) {
+            printf("Packet length was %d\n", nPacketLength);
+ 
+            /* Might not be a bad idea to pay attention to this return value */
+            fread(theData, 1, nPacketLength, fp);
+        }
+        else {   
+            printf("Skipping %d bytes ahead - packet is too big\n", nPacketLength);
+            fseek(fp, nPacketLength, SEEK_CUR);
+        }
+ 
+        /* At this point, we have read the packet and are onto the next one */
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -25,6 +60,8 @@ int main(int argc, char* argv[])
  
     /* Jump through the rest (aka the other 20 bytes) to get past the global header */
     fseek(fp, 24, SEEK_CUR);
+
+    DumpAllPacketLengths(fp);
 
 
 }
