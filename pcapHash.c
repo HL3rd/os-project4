67ce5568fa1f240fb39883991983443f1d42fc9a
HL3rd <hlopez3rd@gmail.com>
2019-03-19 19:13:59 -0700

    Initial code fixed

--- pcapHash.c
+++ pcapHash.c
@@ -2,15 +2,13 @@
 // CSE34341 - Project4
 // Due: 3/28/2019
 
-#include <getopt.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
 
 int main(int argc, char* argv[])
 {
-  
+
 }
