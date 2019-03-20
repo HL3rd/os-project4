a2862950b1874b13b51226be4373912cbf794911
Bailey Blum <bblum1@nd.edu>
2019-03-19 19:37:19 -0700

    Display magic number and dataset file added

--- pcapHash.c
+++ pcapHash.c
@@ -10,5 +10,21 @@
 
 int main(int argc, char* argv[])
 {
+    FILE *fp;
+    fp = fopen("Dataset-Small.pcap", "r");
+
+    /* Display the Magic Number and skip over the rest */
+ 
+    uint32_t   theMagicNum;
+ 
+    if(fread(&theMagicNum, 4, 1, fp) < 4) {
+      // There was an error
+    }
+ 
+    printf("Magic Number was %X\n", theMagicNum);
+ 
+    /* Jump through the rest (aka the other 20 bytes) to get past the global header */
+    fseek(fp, 24, SEEK_CUR);
+
 
 }
