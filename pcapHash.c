eabd59d11f33b98bd860cbce1e29894949e9b334
Alexandra Lopez <alopez10@nd.edu>
2019-03-26 12:58:03 -0700

    added argument parsing to pcapHash

--- pcapHash.c
+++ pcapHash.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+#include <string.h>  
+#include <getopt.h> 
 #include "functions.h"
 
 double totalBytes = 0; // keeps track of all bytes
@@ -23,8 +25,8 @@ double checkPacketsForDuplicates(struct PacketHolder p) {
     double duplicateBytes = 0;
 
     // Our data matches that in the table
-    if (g_MyBigTable[bucket].bIsValid && memcmp(g_MyBigTable[bucket].byData, p.byData, p.bytes)) {
-        printf("Cache Hit\n\n\n");
+    if (g_MyBigTable[bucket].bIsValid && memcmp(g_MyBigTable[bucket].byData, p.byData, p.bytes) == 0) {
+        // printf("Cache Hit\n\n\n");
         duplicateBytes = p.bytes;
     // Evict and add new data
     } else {
@@ -60,15 +62,15 @@ void DumpAllPacketLengths (FILE *fp)
 
         /* Let’s do a sanity check before reading */
         if(nPacketLength < 128) {
-            printf("Skipping %d bytes ahead - packet is too small\n", nPacketLength);
+            // printf("Skipping %d bytes ahead - packet is too small\n", nPacketLength);
             fseek(fp, nPacketLength, SEEK_CUR);
         }
         else if (nPacketLength > 2400) {
-            printf("Skipping %d bytes ahead - packet is too big\n", nPacketLength);
+            // printf("Skipping %d bytes ahead - packet is too big\n", nPacketLength);
             fseek(fp, nPacketLength, SEEK_CUR);
         }
         else {
-            printf("Packet was a good size.\n");
+            // printf("Packet was a good size.\n");
             /* Might not be a bad idea to pay attention to this return value */
             
             // skip the first 52 bytes of data
@@ -93,23 +95,73 @@ void DumpAllPacketLengths (FILE *fp)
 
 int main(int argc, char* argv[])
 {
-    FILE *fp;
-    fp = fopen("Dataset-Small.pcap", "r+");
+    int c;
+    int level = 2; // is no level if specified, we will run using Level 2
+    int threads = 2; //TODO: specify a default value for threads
+    int min_threads = 2; // minimum number of allowed threads
+    int max_files = 10; // maximum number of processed files
 
-    /* Display the Magic Number and skip over the rest */
+    int option_index = 0;
+    static struct option long_options[] = 
+    {
+        {"level",   required_argument, NULL,  'l'},
+        {"thread",  required_argument, NULL,  't'},
+        {NULL,      0,                 NULL,    0}
+    };
 
-    uint32_t   theMagicNum;
+    int files = argc - 1;
 
-    if(fread(&theMagicNum, 4, 1, fp) < 4) {
-      // There was an error
+    // Parse command line arguments
+    while ((c = getopt_long_only(argc, argv, ":l:t:", long_options, &option_index)) != -1) {
+        switch(c) {
+			case 'l':
+                level = atoi(optarg);
+                files = files - 2;
+                printf("level: %d\n", level);
+                break;
+            case 't':
+                threads = atoi(optarg);
+                files = files - 2;
+                printf("threads: %d\n", threads);
+                break;
+		}
+	}
+
+    // Check number of threads
+    if (threads < min_threads) {
+        printf("error: Minimum of two threads allowed.\n");
+        exit(1);
+    }
+    
+    // Save files to be processed in an array
+    char *filenames[max_files];
+    int i;
+    for (i = 0; i < files; i++) {
+        filenames[i] = argv[argc-files + i];
+        printf("file %d: %s\n", i, filenames[i]);
     }
 
-    printf("Magic Number was %X\n", theMagicNum);
+    for (i = 0; i < files; i++) {
+        FILE *fp;
+        fp = fopen(filenames[i], "r+");
 
-    /* Jump through the rest (aka the other 20 bytes) to get past the global header */
-    fseek(fp, 24, SEEK_CUR);
+        /* Display the Magic Number and skip over the rest */
 
-    DumpAllPacketLengths(fp);
+        uint32_t   theMagicNum;
+
+        if(fread(&theMagicNum, 4, 1, fp) < 4) {
+        // There was an error
+        }
+
+        // printf("Magic Number was %X\n", theMagicNum);
+
+        /* Jump through the rest (aka the other 20 bytes) to get past the global header */
+        fseek(fp, 24, SEEK_CUR);
+
+        DumpAllPacketLengths(fp);   
+        
+        fclose(fp);
+    }
 
     printf("totalBytes: %f\n", totalBytes);
     printf("DuplicateBytes: %f\n", totalDuplicateBytes);
